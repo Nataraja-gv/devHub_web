20eb287a03b241785d1e3031ac070ebf387a5332
Nataraja-gv <nataraja2k@gmail.com>
2025-05-21 16:55:54 +0530

    cmd

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,9 +1,18 @@
 import axios from "axios";
-// import { BASE_URL } from "../config_global";
- 
+let baseURL;
+
+if (window.location.hostname === "localhost") {
+  baseURL = "http://localhost:7777";
+} else {
+  baseURL = "/api";
+}
+
+const api = axios.create({
+  baseURL,
+});
 
 const axiosInstance = axios.create({
-  baseURL:"/api",
+  baseURL: baseURL,
   withCredentials: true,
 });
 
