90d26317cc945deaeb27f5a4b354c93526e7fee3
Nataraja-gv <nataraja2k@gmail.com>
2025-05-29 00:19:03 +0530

    comment_localhost

--- src/config_global.js
+++ src/config_global.js
@@ -1,4 +1,7 @@
 // export const BASE_URL =import.meta.env.VITE_FRONTEND_BASE_URL;
-export const BASE_URL =import.meta.env.VITE_FRONTEND_BASE_URL_PROD;
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? import.meta.env.VITE_FRONTEND_BASE_URL
+    : import.meta.env.VITE_FRONTEND_BASE_URL_PROD;
 
-export const RAZOR_KeyId = import.meta.env.VITE_Razor_key_id
+export const RAZOR_KeyId = import.meta.env.VITE_Razor_key_id;
