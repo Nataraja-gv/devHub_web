40deb903231c29fef2a655e378cf216ac7325b0c
Nataraja-gv <nataraja2k@gmail.com>
2025-05-29 00:09:28 +0530

    cmd

--- src/config_global.js
+++ src/config_global.js
@@ -1,2 +1,4 @@
-export const BASE_URL =import.meta.env.VITE_FRONTEND_BASE_URL;
+// export const BASE_URL =import.meta.env.VITE_FRONTEND_BASE_URL;
+export const BASE_URL =import.meta.env.VITE_FRONTEND_BASE_URL_PROD;
+
 export const RAZOR_KeyId = import.meta.env.VITE_Razor_key_id
